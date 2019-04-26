796b4b8078646ea0485a74f166075c0869029a6c
Konrad <kn.morawiec@gmail.com>
2019-04-26 04:03:07 +0200

    Added routing

--- leaflet-app/src/app/app-routing.module.ts
+++ leaflet-app/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { MenuComponent } from './menu/menu.component';
 
-const routes: Routes = [];
+let routes: Routes;
+routes = [
+  {path: 'login', component: SignInComponent},
+  {path: 'registration', component: SignUpComponent},
+  {path: '', component: MenuComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
